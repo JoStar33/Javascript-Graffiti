1c5fa8b18a630cf7e7751fc66549777d61dca993
HoCG <rhwkd5012@naver.com>
2023-01-02 13:51:36 +0900

    재밌는 this

--- 2022-12-30/this_test3.js
+++ 2022-12-30/this_test3.js
@@ -1,7 +1,9 @@
-//이게 가능한 이유는 화살표 함수에는 ⭐️this가 아예 없기 때문⭐️이다.
+
+//얘는 바로직전 객체에따라 this가 결정되는데, 함수안에 함수로 구조를 잡아버렸으니 this가 어떤것인지 인지를 할 수가 없어진것.
+//따라서 grobal이 찍힌다.
 const helloObject = {
   value: "안녕",
-  objectFuntion: function() {
+  objectFuntion() {
     console.log(this);
     function innerFunction() {
       console.log(this);
@@ -10,12 +12,12 @@ const helloObject = {
   }
 }
 
+
+//이게 가능한 이유는 화살표 함수에는 ⭐️this가 아예 없기 때문⭐️이다.
 //화살표함수는 this를 가지고있지않는다. 이때문에 아래와같은 경우는 objectFuntion의 this를 참조하는것이지, 자체적으로 this를 가지는게 아니라는 것이다.
-
-
 const helloObject2 = {
   value: "안녕",
-  objectFuntion: function() {
+  objectFuntion() {
     console.log(this);
     const innerFunction = () => {
       console.log(this);
