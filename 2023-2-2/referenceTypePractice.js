9a7ef9d97ad81d2b4daae30ee0a5bab97434b05f
HoCG <rhwkd5012@naver.com>
2023-02-02 16:25:56 +0900

    feat: 원시타입, 참조타입 공부내용

---
+++ 2023-2-2/referenceTypePractice.js
@@ -0,0 +1,13 @@
+//다음과 같이 참조타입인 객체를 변경할때는 newPerson이라는 변수가 person을 참조하여 같은 메모리를 바라보기 때문에 newPerson이라는 값 자체도 같이 바뀌게 된다.
+
+let person = {
+  name: 'hi',
+  age: 23,
+  place: 'anyang'
+};
+
+let newPerson = person;
+
+person.name = 'jojo';
+
+console.log(newPerson);
