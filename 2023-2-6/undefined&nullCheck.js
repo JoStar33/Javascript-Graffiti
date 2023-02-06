3e5fcea54a5ac4df63fc70241a6a96f3d58ff6db
[아이엔소프트] 황호세 <hshwang@in-soft.co.kr>
2023-02-06 13:45:34 +0900

    chore: 설명 추가

--- 2023-2-6/undefined&nullCheck.js
+++ 2023-2-6/undefined&nullCheck.js
@@ -1,10 +1,14 @@
+
+//case 1
 let data1 = null;
 if(data1 === undefined || data1 === null) {
   data1 = 3;
 }
 
+//case 2
 let data2 = null;
 data2 ??= 3;
 
+//놀랍게도 위에 두 케이스는 동일하다.
 console.log('data1: ', data2);
 console.log('data2: ', data2);
