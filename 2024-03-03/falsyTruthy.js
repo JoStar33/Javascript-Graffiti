d29fef194411bc397db54d91d58fa98e0154c04b
HoCG <rhwkd5012@naver.com>
2024-03-03 22:14:12 +0900

    feat: truthy & falsy

---
+++ 2024-03-03/falsyTruthy.js
@@ -0,0 +1,22 @@
+//falsy
+const one = NaN;
+const two = false;
+const three = 0;
+const four = "";
+const five = null;
+const six = undefined;
+
+console.log(!!one);
+console.log(!!two);
+console.log(!!three);
+console.log(!!four);
+console.log(!!five);
+console.log(!!six);
+/**
+ * false
+ * false
+ * false
+ * false
+ * false
+ * false
+ */
