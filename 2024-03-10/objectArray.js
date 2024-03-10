c28d6f8dabe7205b1916f8f519bbc8c0bd795cc0
HoCG <rhwkd5012@naver.com>
2024-03-10 23:45:19 +0900

    feat: 객체를 배열로 치완해보자.

---
+++ 2024-03-10/objectArray.js
@@ -0,0 +1,10 @@
+const testObject = {
+  a: "aa",
+  b: "bb",
+  c: "cc",
+};
+
+const result_entries = Object.entries(testObject);
+
+//[ [ 'a', 'aa' ], [ 'b', 'bb' ], [ 'c', 'cc' ] ]
+console.log(result_entries);
